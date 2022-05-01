eccdf9850f5f9112bf83d62d77bf342435f196a4
Frisia-coder <93887912+Frisia-coder@users.noreply.github.com>
2022-05-02 00:41:56 +0200

    Add files via upload

---
+++ strings/main.py
@@ -0,0 +1,49 @@
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+# Add your code after this line
+
+
+doelpunt_maker_1= "Gullit"
+doelpunt_maker_2= "van Basten"
+
+goal_1= str("32")
+goal_2= str("54")
+
+scorers= doelpunt_maker_1 + goal_1, doelpunt_maker_2 + goal_2
+
+report= f"{doelpunt_maker_1}. scored in the {goal_1}nd minute'\n'{doelpunt_maker_2}. scored in the {goal_2}th minute"
+
+player= "Ronald Koeman"
+
+first_name= player.find("Ronald")
+last_name_len=player.find("Koeman") 
+name_short= "R. Koeman"
+chant= "Ronald!"*6
+
+print(2 != 2)
+print(2 != 3)
+
+print(chant)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
