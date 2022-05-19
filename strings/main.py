b7ba79dcef8502cd80540efbc05af84804d29da2
Frisia-coder <93887912+Frisia-coder@users.noreply.github.com>
2022-05-19 16:54:34 +0200

    Add files via upload

--- strings/main.py
+++ strings/main.py
@@ -4,28 +4,42 @@ __human_name__ = 'strings'
 
 # Add your code after this line
 
+doelpunt_maker_0= "Ruud Gullit"
+doelpunt_maker_1= "Marco van Basten"
+print(doelpunt_maker_0)
+print(doelpunt_maker_1)
 
-doelpunt_maker_1= "Gullit"
-doelpunt_maker_2= "van Basten"
+goal_0= 32
+goal_1= 54
+print(goal_0)
+print(goal_1)
 
-goal_1= str("32")
-goal_2= str("54")
+scorers= doelpunt_maker_0 + " " + str(goal_0) + ','+" " + doelpunt_maker_1 + " " + str(goal_1)
+print(scorers)
 
-scorers= doelpunt_maker_1 + goal_1, doelpunt_maker_2 + goal_2
-
-report= f"{doelpunt_maker_1}. scored in the {goal_1}nd minute'\n'{doelpunt_maker_2}. scored in the {goal_2}th minute"
+report= f"{doelpunt_maker_0} scored in the {goal_0}nd minute\n{doelpunt_maker_1} scored in the {goal_1}th minute"
+print(report)
 
 player= "Ronald Koeman"
+print(player)
 
-first_name= player.find("Ronald")
-last_name_len=player.find("Koeman") 
-name_short= "R. Koeman"
-chant= "Ronald!"*6
+first_name= player[0:6]
+player.find(" ")
+print(first_name)
 
-print(2 != 2)
-print(2 != 3)
+last_name_len = len(player[-player.find(' '):])
+print(last_name_len)
 
+name_short = player[:player.find(' ')].upper()[:1] + '. ' + player[-player.find(' '):]
+print(name_short)
+
+chant = ((player[:player.find(' ')] + '! ') * len(player[:player.find(' ')])).strip()
 print(chant)
+
+good_chant = chant[-1:] != ' '
+print(good_chant)
+
+
 
 
 
